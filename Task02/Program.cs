64f63582bd364cf890e500f9935d7d01a09573b9
Mariia <mashulkaegor@mail.ru>
2022-08-24 21:48:54 +0400

    add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2. Слоник
+Console.WriteLine("Введите значение x: ");
+int x = int.Parse(Console.ReadLine());
+int result;
+if(x%5 != 0)
+{
+    result = x/5 + 1;
+}
+else
+{
+    result = x/5;
+}
+Console.WriteLine($"Понадобится шагов: {result}");
