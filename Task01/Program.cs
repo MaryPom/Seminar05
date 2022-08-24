baa0f9c3db7b8d16a9df7f5b8faf74613b2a4466
Mariia <mashulkaegor@mail.ru>
2022-08-24 21:46:31 +0400

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1. Про театральную площадь
+int n = 6;
+int m = 6;
+int a = 4;
+int x1;
+int x2;
+if(n%a != 0) 
+{
+    x1 = n/a+1;
+}
+else
+{
+    x1 = n/a;
+}
+if(m%a != 0) 
+{
+    x2 = m/a+1;
+}
+else
+{
+    x2 = m/a;
+}
+
+Console.WriteLine($"Понадобится плиток: {x1*x2}");
